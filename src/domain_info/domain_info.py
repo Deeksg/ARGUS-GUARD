533eec7c69ffce23e5189f97c6bafa374885fea8
Deeksha Gupta <kg.deeksha2014@gmail.com>
2026-01-04 20:37:47 +0530

    Add requests dependency for HTTP inspection

---
+++ src/domain_info/domain_info.py
@@ -0,0 +1,4 @@
+#input info = domain name 
+#output info = IP Address(only primary 1 right now), Server headers, HTTP Status
+#possible errors = domains not reachable, invalid domain name, no HTTP response
+
