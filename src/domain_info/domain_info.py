32e01d0f93effbbc3c2686da2694fef24b05cbe7
Deeksha Gupta <kg.deeksha2014@gmail.com>
2026-01-05 14:24:59 +0530

    Add HTTP status and server detection

--- src/domain_info/domain_info.py
+++ src/domain_info/domain_info.py
@@ -3,6 +3,7 @@
 #possible errors = domains not reachable, invalid domain name, no HTTP response
 
 import socket
+import requests
 
 def get_ip_from_domain(domain_name):
     try:
@@ -10,6 +11,19 @@ def get_ip_from_domain(domain_name):
         return ip_address
     except socket.gaierror:
         return None
+
+def get_httpStatusCode_and_serverHeader(domain_name):
+    url="http://"+ domain_name 
+    try:
+        response=requests.get(url, timeout=5)
+        status_code=response.status_code
+        server_header=response.headers.get('server')
+        http_status={"status_code":status_code, "server_header":server_header}
+        return http_status
+    except Exception as err:
+        return None
+
+
     
 def main():
     domain_name= input("Enter domain name:")
@@ -18,6 +32,17 @@ def main():
         print(f"The IP address for {domain_name}={ip_address}")
     else:
         print(f"Couldn't find IP for {domain_name}")
+
+    http_status=get_httpStatusCode_and_serverHeader(domain_name)
+    if(http_status):
+        if(http_status['server_header']!=None):
+            print(f"The http status code for {domain_name}={http_status['status_code']}")
+            print(f"The server header for {domain_name}={http_status['server_header']}")
+        else:
+            print(f"The http status code for {domain_name}={http_status['status_code']}")
+            print(f"The server header for {domain_name} is NOT DISCLOSED")
+    else:
+        print(f"Couldn't find http status for {domain_name}")
 
 if __name__ == "__main__":
     main()
