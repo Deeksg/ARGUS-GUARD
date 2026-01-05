cde9fe31ec57740e88b11a53b898cdbb6c252cfe
Deeksha Gupta <kg.deeksha2014@gmail.com>
2026-01-05 15:08:55 +0530

    Add HTTPS fallback for HTTP status and server detection

--- src/domain_info/domain_info.py
+++ src/domain_info/domain_info.py
@@ -13,15 +13,23 @@ def get_ip_from_domain(domain_name):
         return None
 
 def get_httpStatusCode_and_serverHeader(domain_name):
-    url="http://"+ domain_name 
     try:
+        url="https://"+ domain_name 
         response=requests.get(url, timeout=5)
         status_code=response.status_code
         server_header=response.headers.get('server')
-        http_status={"status_code":status_code, "server_header":server_header}
+        http_status={"protocol":"https","status_code":status_code, "server_header":server_header}
         return http_status
-    except Exception as err:
-        return None
+    except:
+        try:
+            url="http://"+ domain_name 
+            response=requests.get(url, timeout=5)
+            status_code=response.status_code
+            server_header=response.headers.get('server')
+            http_status={"protocol":"http","status_code":status_code, "server_header":server_header}
+            return http_status
+        except Exception as err:
+            return None
 
 
     
@@ -36,9 +44,11 @@ def main():
     http_status=get_httpStatusCode_and_serverHeader(domain_name)
     if(http_status):
         if(http_status['server_header']!=None):
+            print(f"The protocol used for {domain_name}={http_status['protocol']}")
             print(f"The http status code for {domain_name}={http_status['status_code']}")
             print(f"The server header for {domain_name}={http_status['server_header']}")
         else:
+            print(f"The protocol used for {domain_name}={http_status['protocol']}")
             print(f"The http status code for {domain_name}={http_status['status_code']}")
             print(f"The server header for {domain_name} is NOT DISCLOSED")
     else:
